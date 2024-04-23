b3925413e33189e04ee85838b3f674e129e6b18f
Miroslav <157689323+MiroslavTretiak@users.noreply.github.com>
2024-04-23 10:57:56 +0300

    Angular

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { SkaiciuokleComponent } from './skaiciuokle/skaiciuokle.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, SkaiciuokleComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
